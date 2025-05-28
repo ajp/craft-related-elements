c21351f899ae4dac9cd121df3269027d03db4df7
MindseekerMedia <info@mindseeker.media>
2025-05-29 00:43:22 +0200

    feat: add support for displaying element type labels
    - Introduced a new setting `showElementTypeLabel` to control the display of element type labels (Entry, Category, Asset, Tag) next to related elements.
    - Updated the settings template to include a toggle for the new feature.

--- src/models/Settings.php
+++ src/models/Settings.php
@@ -8,11 +8,12 @@ class Settings extends Model
 {
     public bool $enableNestedElements = true;
     public int $initialLimit = 10;
+    public bool $showElementTypeLabel = true;
 
     public function rules(): array
     {
         return [
-            [['enableNestedElements'], 'boolean'],
+            [['enableNestedElements', 'showElementTypeLabel'], 'boolean'],
             [['initialLimit'], 'integer', 'min' => 1, 'max' => 100],
             [['initialLimit'], 'default', 'value' => 10],
         ];
